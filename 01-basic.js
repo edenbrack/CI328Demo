54850341f1803ef3bf1c671f0b9506e8c856ca64
Eden Brackenbury <e.brackenbury1@uni.brighton.ac.uk>
2018-11-23 14:13:42 +0000

    create party scene

--- 01-basic.js
+++ 01-basic.js
@@ -12,13 +12,14 @@ function main() {
     var game = new Phaser.Game(config);
 }
 
-function create() {
-    var text = this.add.text(50, 50, "Phaser is working!", { fontSize: '32px', fill: '#FFF' });
-}
-
 function preload() {
     game.load.image('kitchen', 'images/kitchen.png);
     game.load.image('diningroom', 'images/diningroom.png);
     game.load.image('party', 'images/xmas.png);
 }
 
+function create() {
+    var text = this.add.text(50, 50, "Phaser is working!", { fontSize: '32px', fill: '#FFF' });
+        
+    game.add.sprite(0,0, 'party');
+}
