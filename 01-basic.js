c974499b3385685512e9af8655e3a931bb7d3eda
Eden Brackenbury <e.brackenbury1@uni.brighton.ac.uk>
2018-11-29 11:19:29 +0000

    preload and create sprite

--- 01-basic.js
+++ 01-basic.js
@@ -7,10 +7,11 @@ function preload() {
     game.load.image('kitchen', 'images/kitchen.png');
     game.load.image('diningroom', 'images/diningroom.png');
     game.load.image('party', 'images/xmas.png');
+    game.load.spritesheet('character', 'images/sprite.png', 32, 63, 7);
 }
 
 function create() {
     var text = this.add.text(50, 50, "Phaser is working!", { fontSize: '32px', fill: '#FFF' });
         
-    game.add.sprite(0, 0, 'party');
+    game.add.sprite(0, 0, 'character');
 }
